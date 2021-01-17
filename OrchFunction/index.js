18b71c47429ffe65021c1fe35c9ed1f808d87eca
sean <srcox86@gmail.com>
2021-01-17 07:43:09 -0600

    Added code to orchestrator function.

--- OrchFunction/index.js
+++ OrchFunction/index.js
@@ -1,24 +1,14 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an HTTP starter function.
- * 
- * Before running this sample, please:
- * - create a Durable activity function (default name is "Hello")
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your 
- *    function app in Kudu
- */
-
-const df = require("durable-functions");
+﻿const df = require("durable-functions");
 
 module.exports = df.orchestrator(function* (context) {
     const outputs = [];
 
-    // Replace "Hello" with the name of your Durable Activity Function.
-    outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
-    outputs.push(yield context.df.callActivity("Hello", "Seattle"));
-    outputs.push(yield context.df.callActivity("Hello", "London"));
+    /*
+    * We will call the approval activity with a reject and an approved to simulate both
+    */
 
-    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    outputs.push(yield context.df.callActivity("Approval", "Approved"));
+    outputs.push(yield context.df.callActivity("Approval", "Rejected"));
+
     return outputs;
 });
